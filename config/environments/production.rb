982aa0cdcf79f7c1380bc303eb0a768c18a50e42
vinhbt241 <vinhbuith32412000@gmail.com>
2024-10-09 20:41:58 +0700

    config action cable connection for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
+  config.action_cable.disable_request_forgery_protection = true
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   config.assume_ssl = false
