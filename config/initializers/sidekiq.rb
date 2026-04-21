739ce38c7fd474a3b4823820eb24255682392dd2
Vinh <vinhbuith32412000@gmail.com>
2026-04-21 10:47:08 +0700

    update Dockerfile for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
-sidekiq_config = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+redis_url =
+  ENV['REDIS_URL'] ||
+  Rails.application.credentials.dig(:redis, :url)
+
+if redis_url.nil? && Rails.env.production?
+  # Allow asset precompile (build phase) to pass
+  raise('Missing Redis URL') unless ENV['SECRET_KEY_BASE_DUMMY']
+
+  redis_url = 'redis://localhost:6379/1'
+end
+
+sidekiq_config = {
+  url: redis_url,
+  network_timeout: 5,
+  pool_timeout: 5,
+  size: ENV.fetch('RAILS_MAX_THREADS', 5).to_i
+}
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
